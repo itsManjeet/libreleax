64295a8b71a088be95a4ab35a393009df083ae6f
itsmanjeet <itsmanjeet1998@gmail.com>
2019-12-06 22:36:31 +0000

    updated test_app type

--- test/test_app.cc
+++ test/test_app.cc
@@ -1,54 +1,71 @@
 #include <releax/releax.hh>
 #include <releax/app.hh>
 
+/* test_app : sample app to demonstracte the usage of app.hh
+ * app.hh is a releax api to easily build cli apps for releax os
+ */
+
 using namespace std;
 
 error
-arg1_exec(vector<string> args,
-          vector<string> flags) {
-    
-    cout << "flags: ";
-    for (auto f: flags) {
-        cout << f << ", ";
-    }
-    cout << endl;
-    cout << "args: " ;
-    for (auto a: args) {
-        cout << a << ", ";
-    }
-    cout << endl;
-    error err;
-    return err;
+wget_main(vector<string> args,
+         vector<string> flags)
+{
+    cout << "download from url: " << endl;
+
+    return error(0,"success");
 }
 
-int main(int argc, char** argv) {
-    App test_app = App(
-        "testApp",
-        0.1, 'a',
-        "this is a sample app"
+
+error
+wget_sub_from_ftp(vector<string> args,
+                  vector<string> flags)
+{
+    cout << "download ftp from url: " <<  endl;
+    return error(0,"success");
+}
+
+int
+main (int   argc, 
+     char** argv)
+{
+    /* Declare new app */
+    auto wget = App(
+        "wget",                       // app name
+        0.1 , 'a',                    // 0.1 = major.minor version |  'a' = release [a = alpha, b = beta, s = stable]
+        "wget is a simple implementation of unix wget with app.hh releax api"  // descriptions for app
     );
 
-    test_app.add_author(
+    /* Add Authors 
+     * add_author can add any number of authors for the app
+     */
+    wget.add_author(
         "Manjeet Saini",
         "itsmanjeet@releax.in",
-        "Lead Developer"
+        "Lead Developer at releax os"
     );
 
-    test_app.add_author(
-        "another author",
+    wget.add_author(
+        "Another Author",
         "another@releax.in",
-        "Side Developer"
-    );
-    test_app.add_sub(
-        "arg1",
-        "Sample argument",
-        "[ARGS] filename",
-        arg1_exec
+        "Another Author for testing"
     );
 
-    error err = test_app.execute(argc, argv);
-    if (err.first != 0) {
-        cout << err.second << endl;
+    /* Main Function */
+    wget.func = wget_main;
+
+    /* Sub commands */
+    wget.add_sub(
+        "from-ftp",                                 // Argument name from which you want to call
+        "to Download all file from ftp addresss",   // Sub Command description
+        "[URL] -o [OUTPUT]",                        // Usage for help
+        wget_sub_from_ftp
+    );
+    // Similarly more sub commands can be add
+
+    /* Execute Command*/
+    error err = wget.execute(argc,argv);
+    if (err.status != 0) {
+        cout << "err: " << err.message << endl;
     }
 }
-
