5125f56dba1f301f481231e6be2658f37c65bf59
itsmanjeet <itsmanjeet1998@gmail.com>
2019-12-06 23:36:03 +0000

    fixed for disk

--- test.sh
+++ test.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-g++ test/test_$1.cc -o build/test_$1 -I api -L build -lreleax 
+if [[ $1 == "disk" ]] ; then 
+    flags="-lblkid"
+fi
+g++ test/test_$1.cc -o build/test_$1 -I api -L build -lreleax $flags
 
 LD_LIBRARY_PATH=build/ build/test_$1 $2
