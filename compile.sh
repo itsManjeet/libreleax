f2e5728baa2024f0f94201aa600abd6c777c88e1
itsmanjeet <itsmanjeet1998@gmail.com>
2019-12-07 22:04:18 +0000

    updated for gtkmm ui

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-g++ $(find src -name "*.cc" -type f) -o build/libreleax.so -fPIC -shared -I api/ -fpermissive
+g++ $(find src -name "*.cc" -type f) -o build/libreleax.so -fPIC -shared -I api/ -fpermissive `pkg-config --libs --cflags gtkmm-3.0`
--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-g++ test/test_$1.cc -o build/test_$1 -I api -L build -lreleax -lblkid
+g++ test/test_$1.cc -o build/test_$1 -I api -L build -lreleax -lblkid `pkg-config --libs --cflags gtkmm-3.0`
 
 LD_LIBRARY_PATH=build/ build/test_$1 $2
