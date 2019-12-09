7d027f11f529db12d733cd859e97e77887b4407d
itsmanjeet <itsmanjeet1998@gmail.com>
2019-12-09 21:29:37 +0000

    more responsive build system

--- compile.sh
+++ compile.sh
@@ -1,3 +1,18 @@
 #!/bin/sh
 
-g++ $(find src -name "*.cc" -type f) -o build/libreleax.so -fPIC -shared -I api/ -fpermissive `pkg-config --libs --cflags gtkmm-3.0` -lstdc++fs -std=c++17
+CPP="g++"
+CPFLAGS="-fPIC -shared -I api/ -fpermissive `pkg-config --libs --cflags gtkmm-3.0` -lstdc++fs -std=c++17 -lcurl"
+[ -d build ] && rm -rf build
+
+mkdir -p build/{test,cache}
+
+for lib in src/* ; do
+    echo "Building $(basename $lib)"
+    for f in $lib/*.cc ; do
+        echo "    CC $(basename ${f%.*})"
+        $CPP -c $f -o build/cache/$(basename ${f%.*}).o $CPFLAGS
+    done
+done
+
+$CPP -o build/libreleax.so build/cache/*.o $CPFLAGS
+rm -rf build/cache
