92926cf53dbc84fef0f391a15de0e109010cef10
itsmanjeet <itsmanjeet1998@gmail.com>
2019-12-11 21:05:03 +0000

    added: filesys::copy - to copy file from source to destination

--- api/releax/filesys.hh
+++ api/releax/filesys.hh
@@ -5,7 +5,9 @@
 
 namespace filesys {
     int makedir(std::string path, mode_t mode);
+    int copy(std::string source, std::string dest);
     bool exist(std::string path);
+    std::list<std::string> list_all(std::string path);
 }
 
 #endif
--- src/filesys/filesys.cc
+++ src/filesys/filesys.cc
@@ -1,6 +1,10 @@
 #include <releax/filesys.hh>
 #include <sys/stat.h>
 
+#include <filesystem>
+
+namespace fs = std::filesystem;
+
 int
 filesys::makedir(std::string path, mode_t mode)
 {
@@ -23,6 +27,21 @@ filesys::makedir(std::string path, mode_t mode)
     return mdret;
 }
 
+int
+filesys::
+copy(std::string source,
+     std::string dest)
+{
+    std::ifstream src(source);
+    std::ofstream dst(dest);
+
+    dst << src.rdbuf();
+
+    src.close();
+    dst.close();
+
+    return 0;
+}
 
 bool
 filesys::exist(std::string path)
@@ -32,4 +51,15 @@ filesys::exist(std::string path)
     if (stat(path.c_str(), &sb) != -1)
         return true;
     return false;
+}
+
+
+std::list<std::string>
+filesys::list_all(std::string path)
+{
+    std::list<std::string> all_content;
+    for (const auto &entry : fs::directory_iterator(path)) {
+        all_content.push_front(entry.path().filename());
+    }
+    return all_content;
 }
