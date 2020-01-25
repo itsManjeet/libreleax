4f868835e8e6fc3c591e409f5444b55f909bb825
itsmanjeet <itsmanjeet1998@gmail.com>
2020-01-25 21:53:24 +0530

    fixed releax.hh for 0.2.b

--- api/releax/releax.hh
+++ api/releax/releax.hh
@@ -2,13 +2,40 @@
 #define RELEAX_HH
 
 #include <bits/stdc++.h>
+#include <releax/config.hh>
+#include <releax/filesys.hh>
 
-class error {
-public:
-    int         status;
-    std::string message;
+#define OS_INFO_FILE "/etc/os-release"
 
-    error(int         status,
-          std::string message);
-};
+#define DEFAULT_OS_NAME         "releax"
+#define DEFAULT_OS_VERSION      0.2
+#define DEFAULT_OS_VER_CODENAME "beta"
+#define DEFAULT_OS_ID           "releax"
+
+
+namespace osinfo {
+    typedef enum {
+        alpha_release,
+        beta_release,
+        stable_release,
+        unknown_release
+    } release_t;
+    
+    typedef float version_t;
+
+    typedef struct {
+        std::string name,
+                    id,
+                    pretty_name,
+                    version_id,
+                    home_url,
+                    version_code;
+        
+        version_t   version;
+        release_t   release;
+    } osinfo_t ;
+
+    osinfo_t get_info();
+}
+
 #endif
