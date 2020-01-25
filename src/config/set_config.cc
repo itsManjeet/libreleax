2cd653f0191ef5ecd9bbb777be0a266399ca2546
itsmanjeet <itsmanjeet1998@gmail.com>
2020-01-25 21:54:44 +0530

    added set_config.cc

---
+++ src/config/set_config.cc
@@ -0,0 +1,49 @@
+#include <releax/config.hh>
+
+int
+Config::set(std::string variable,
+            std::string value)
+{
+    std::ifstream infile(this->filename);
+    if (!infile.good()) {
+        return -5;
+    }
+    std::ofstream outfile(this->filename + ".new");
+    if (!infile.good()) {
+        return -6;
+    }
+
+    std::string line;
+    bool varfound = false;
+    for (auto a = getline(infile, line) != 0) {
+        if (line.find("=") != line.npos) {
+            if (!varfound) {
+                std::string tmp_var = this->trim(line.substr(0,line.find("=")));
+                if (tmp_var == variable) {
+                    std::string input_line = variable + " = " + value;
+                    outfile.write(input_line,input_line.size());
+                    varfound = true;
+                } else {
+                    outfile.write(line,line.size());
+                }
+            } else {
+                outfile.write(line,line.size());
+            }
+
+        }
+        
+    }
+
+    if (!varfound) {
+        std::string input_line = variable + " = " + value;
+        outfile.write(input_line,input_line.size());
+    }
+
+    outfile.close();
+    infile.close();
+
+    filesys::copy(this->filename, this->filename + ".old");
+    filesys::copy(this->filename + ".new", this->filename);
+
+    return 0;
+}
