3bd5732afe0f467267722c4673ba5e4de0e9bda9
itsmanjeet <itsmanjeet1998@gmail.com>
2020-01-25 21:53:24 +0530

    fixed config.hh for 0.2.b

--- api/releax/config.hh
+++ api/releax/config.hh
@@ -6,10 +6,12 @@
 class Config {
 private:
     std::string filename;
+    std::string trim(const std::string &str);
 public:
     Config(std::string filename);
     std::string get(std::string variable, std::string dflt);  
-    std::string get(std::string variable); 
+    std::string get(std::string variable);
+    int         set(std::string variable, std::string val);
 };
 
 #endif
